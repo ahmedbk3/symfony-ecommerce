8aff21ed250f8ad2e24479dbb0d2ece3666f0993
ahmedbk3 <124940475+ahmedbk3@users.noreply.github.com>
2025-06-01 20:40:26 +0100

    DEV TROUBLESHOOTING
    Cela forcera la variable d’environnement APP_SECRET pour ta session PowerShell.
    
    meme pour la variable DATABASE_URL   DANS .ENV POUR EVITER LES ERREURS

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,12 +1,7 @@
-<?php
-// ───────────────────────────────────────────────────────────────────────────────
-// Suppress E_DEPRECATED and E_STRICT warnings before Symfony’s ErrorHandler loads
-error_reporting(E_ALL & ~E_DEPRECATED & ~E_STRICT);
-ini_set('display_errors', '1');
-// ───────────────────────────────────────────────────────────────────────────────
-
 use Symfony\Component\Dotenv\Dotenv;
 
-// If you use a .env file, uncomment the lines below. Otherwise leave them commented.
-// require dirname(__DIR__).'/.env';
-// (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+require dirname(__DIR__).'/vendor/autoload.php';
+
+if (method_exists(Dotenv::class, 'bootEnv')) {
+    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+}
