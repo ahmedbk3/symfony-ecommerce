00bb28179b46e69940984be20929944f00f7cbbd
ahmedbk3 <124940475+ahmedbk3@users.noreply.github.com>
2025-06-01 15:47:07 +0100

    fixing admin controleer to manage clients and products

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -1,7 +1,7 @@
-<?php
-
 namespace App\Controller;
 
+use App\Repository\UserRepository;
+use App\Repository\ProductRepository; // <-- change here
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +9,40 @@ use Symfony\Component\Routing\Annotation\Route;
 class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'admin_dashboard')]
-    public function index(): Response
+    public function index(UserRepository $userRepository, ProductRepository $productRepository): Response
     {
-        return $this->render('admin/index.html.twig');
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        $usersCount = $userRepository->count([]);
+        $productsCount = $productRepository->count([]);
+
+        return $this->render('admin/index.html.twig', [
+            'usersCount' => $usersCount,
+            'productsCount' => $productsCount,  // renamed
+        ]);
+    }
+
+    #[Route('/admin/users', name: 'admin_manage_users')]
+    public function manageUsers(UserRepository $userRepository): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        $users = $userRepository->findAll();
+
+        return $this->render('admin/manage_users.html.twig', [
+            'users' => $users,
+        ]);
+    }
+
+    #[Route('/admin/products', name: 'admin_manage_products')]  // changed route name
+    public function manageProducts(ProductRepository $productRepository): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        $products = $productRepository->findAll();
+
+        return $this->render('admin/manage_products.html.twig', [  // changed template name
+            'products' => $products,
+        ]);
     }
 }
