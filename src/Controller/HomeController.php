33ec00b7c48abb42ede69a32fe3abb53167f9600
ahmedbk3 <124940475+ahmedbk3@users.noreply.github.com>
2025-06-04 14:54:56 +0100

    last touches
    adeed some style and a demo video

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ProductRepository;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_homepage')]
-    public function index(): Response
+    public function index(ProductRepository $productRepository): Response
     {
+         // Find 3 featured products ordered by latest
+        $products = $productRepository->findBy([], ['id' => 'ASC'], 3);
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'featuredProducts' => $products,
         ]);
     }
 }
